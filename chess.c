45d29773aba43125e6ae74efd2712e016b28d7d4
JoseLuisCalizaya <joseluisataqu@gmail.com>
2024-11-09 22:56:18 -0500

    Se imprime la direccion de memoria de la variable con identificador newFig de la funcion reverse() donde se hace evidente que tiene una direccion de memoria diferente con respecto a la direccion de memoria que se quiere liberar y tambien a la registrada. Se tiene la sospechosa de que el error se encuentra en la funcion allocateMemory y UnlinkMemory ya que comparten el mismo contenido pero no la direccion de memoria, por lo que es una hipotesis

--- chess.c
+++ chess.c
@@ -5,21 +5,23 @@
 
 char** allocateMemory(int rows, size_t cols){
   char** newFig;
-  memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1));
+  memoryAlloc((void**)&newFig, sizeof(char*)*(rows + 1)); 
+  //&newFig = &(**newFig)
   fprintf(stderr,"[DEBUG] Memory for newFig : %p\n", &newFig);
   for(int i = 0; i < rows; i++){
     memoryAlloc((void**)&newFig[i], sizeof(char)*(cols + 1));
+  //&newFig[i] == &(*(*(newFig[i])))
     fprintf(stderr, "[DEBUG] Memory for string : %p\n", &newFig[i]);
   }
   return newFig;
 }
 
-void unlinkMemory(char** fig){
+void unlinkMemory(char** fig){ //fig = newFig (variables distintas)
   countMemoryEntries();
   for(int i = 0; fig[i]; i++)
-    unregisterPointer((void**)&fig[i]);
+    unregisterPointer((void**)&fig[i]); //fig[i] == newFig[i] (mismo contenido)
   countMemoryEntries();
-  unregisterPointer((void**)&fig);
+  unregisterPointer((void**)&fig); //  &fig != &newFig (memorias distintas)
   countMemoryEntries();
 }
 
@@ -31,7 +33,7 @@ char** reverse(char** fig){
   while(fig[0][++cols]);
 
   char** newFig = allocateMemory(rows, cols);
-
+  fprintf(stderr, "Memory of newFig in reverse function: %p\n", &newFig);
   for(int i = 0; fig[i]; i++){
     for(int j = 0; fig[0][j]; j++)
       newFig[i][j] = fig[i][j];
@@ -41,4 +43,3 @@ char** reverse(char** fig){
   unlinkMemory(newFig);
   return newFig;
 }
-
